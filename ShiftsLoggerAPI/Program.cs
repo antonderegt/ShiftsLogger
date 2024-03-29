83c0a7b8dcd24cb4b66bc21641c29a8411fcdc00
Anton De Regt <antonderegt@pm.me>
2024-03-29 14:22:57 +0100

    Add swagger

--- ShiftsLoggerAPI/Program.cs
+++ ShiftsLoggerAPI/Program.cs
@@ -2,10 +2,18 @@ using ShiftsLoggerAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 builder.Services.AddSingleton<IShiftService>(new SqlServerShiftService(new ShiftsContext(builder.Configuration)));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/shifts", ShiftController.GetShiftsAsync);
 
